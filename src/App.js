a91d6658c46b16a1406aaf8e13865c6dcf0588c9
Tereshkov Sergei[D] <rat87@mail.ru>
2021-07-02 22:43:02 +0300

    feat: add App module

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import st from './App.module.css';
+import AppRouter from './components/AppRouter';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={st.app}>
+      <BrowserRouter>
+        <Switch>
+          <PrivateRoute path="/dashboard" permited={true} component={AppRouter} />
+          <Route path="/" component={LoginPage} exact />
+          <Redirect to="/" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
+
+const PrivateRoute = ({ component: Component, permited, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) => (permited ? <Component {...props} /> : <Redirect to="/" />)}
+  />
+);
 
 export default App;
