06dd7fea875445fad01216cb828a6fce67c269c3
Tereshkov Sergei[D] <rat87@mail.ru>
2021-07-05 02:31:32 +0300

    fix: fix server address

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
 // Top secret config file :)
 // It visible for demo purposes only.
 
-let serv;
-if (process.env.NODE_ENV === 'production') {
-  serv = 'https://lighting-control-app.herokuapp.com';
-}
+let serv = 'https://lighting-control-app.herokuapp.com';
+
 if (process.env.NODE_ENV === 'development') {
   serv = 'http://localhost';
 }
