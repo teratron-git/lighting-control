24d79e500df3a32158f85b68b56323105b51488b
Tereshkov Sergei[D] <rat87@mail.ru>
2021-07-05 17:17:51 +0300

    glitch

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 // Top secret config file :)
 // It visible for demo purposes only.
 
-let serv = 'https://lighting-control-app.herokuapp.com';
+let serv = 'https://platinum-candy-responsibility.glitch.me';
 
-// if (process.env.NODE_ENV === 'development') {
-//   serv = 'http://localhost:3001';
-// }
+if (process.env.NODE_ENV === 'development') {
+  serv = 'http://localhost:3001';
+}
 
 module.exports = {
   serv: serv,
