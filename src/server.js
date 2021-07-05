e4b1e03a872b9b16c07962e11cd228754f8c1256
Tereshkov Sergei[D] <rat87@mail.ru>
2021-07-05 19:06:10 +0300

    new

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 // Top secret config file :)
 // It visible for demo purposes only.
 
-let serv = 'https://platinum-candy-responsibility.glitch.me';
+let serv = 'https://amazing-daffodil-pail.glitch.me';
 
 // if (process.env.NODE_ENV === 'development') {
 //   serv = 'http://localhost:3001';
